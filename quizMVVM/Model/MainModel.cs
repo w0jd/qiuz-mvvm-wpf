21bd3aa605238372c6a16ae800b59e0df85fbe05
w0jd <dwoitek1@gmail.com>
2023-04-26 19:33:36 +0200

    początek modelu i połączenie

--- quizMVVM/Model/MainModel.cs
+++ quizMVVM/Model/MainModel.cs
@@ -7,10 +7,22 @@ using System.Linq;
 using System.Data.SQLite;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace quizMVVM.Model
 {
+    internal class quiz{
+        
+        int id;
+        string nazwa;
+    }
+    internal class pytania
+    {
+        int id_pyt;
+    }
     internal class MainModel
     {
+        static SQLiteConnection conn = new SQLiteConnection(@"Data Source=quiz.db;Version=3");
+
     }
 }
