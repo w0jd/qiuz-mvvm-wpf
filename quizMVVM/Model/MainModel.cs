62955faccbd4d23a643d8ac2e0e09a8cbcf1d33c
w0jd <dwoitek1@gmail.com>
2023-04-26 20:58:00 +0200

    dodano w modelu klasy i przekopiowano relaycommand

--- quizMVVM/Model/MainModel.cs
+++ quizMVVM/Model/MainModel.cs
@@ -11,28 +11,15 @@ using System.ComponentModel;
 
 namespace quizMVVM.Model
 {
-    internal class quiz {
-
+    internal class quiz{
+        
         int id;
         string nazwa;
-        string privteId;
-        string publicId;
     }
     internal class pytania
     {
         int id_pyt;
-        string tresc;
-        string odp1;
-        string odp2;
-        string odp3;
-        string odp4;
-        int IdQuiz;
-
     }
-/*    internal class listOfQuestions(pytania obj)
-        {
-
-        }*/
     internal class MainModel
     {
         static SQLiteConnection conn = new SQLiteConnection(@"Data Source=quiz.db;Version=3");
