5c3cd7d78eb1ec4d8ad83f6967b921e10cf4f6fc
w0jd <dwoitek1@gmail.com>
2023-04-26 19:58:09 +0200

    zarys klas

--- quizMVVM/Model/MainModel.cs
+++ quizMVVM/Model/MainModel.cs
@@ -11,15 +11,28 @@ using System.ComponentModel;
 
 namespace quizMVVM.Model
 {
-    internal class quiz{
-        
+    internal class quiz {
+
         int id;
         string nazwa;
+        string privteId;
+        string publicId;
     }
     internal class pytania
     {
         int id_pyt;
+        string tresc;
+        string odp1;
+        string odp2;
+        string odp3;
+        string odp4;
+        int IdQuiz;
+
     }
+    internal class listOfQuestions(pytania)
+        {
+
+        }
     internal class MainModel
     {
         static SQLiteConnection conn = new SQLiteConnection(@"Data Source=quiz.db;Version=3");
