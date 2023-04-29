81ccdbf128a9fb8ddaf56cc669bfdb1705a6f45c
w0jd <dwoitek1@gmail.com>
2023-04-29 13:41:11 +0200

    działa połączenie z bazą tylko dla pełnej ścieżki
    działa połączenie z bazą danych jeśli poda się pełną ścieżkę w kodzie pobiera też dane i dopisuje do obiektu

--- quizMVVM/Model/MainModel.cs
+++ quizMVVM/Model/MainModel.cs
@@ -5,12 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SQLite;
 using System.ComponentModel;
+using System.Collections;
+using System.IO;
 
 namespace quizMVVM.Model
 {
     internal class quiz{
-        
-        int id;
+       public quiz(Int64 id ,string nazwa)
+        {
+
+            this.id = id;
+            this.nazwa = nazwa;
+        }
+        Int64 id;
         string nazwa;
     }
     internal class pytania
@@ -19,7 +26,32 @@ namespace quizMVVM.Model
     }
     internal class MainModel
     {
-        static SQLiteConnection conn = new SQLiteConnection(@"Data Source=quiz.db;Version=3");
+/*        static string lok = Directory.GetCurrentDirectory()+"quiz.db";
+        static SQLiteConnection conn = new SQLiteConnection($@"Data Source={lok};Version=3");*/
+       public static ArrayList ShowAllQuizes(SQLiteConnection conn)
+        {
+
+            SQLiteDataReader reader;
+            SQLiteCommand command;
+            conn.Open();
+            string currentDirectory = Directory.GetCurrentDirectory();
+            Console.WriteLine(currentDirectory);
+            command = conn.CreateCommand();
+            command.CommandText = "SELECT * FROM quizy";
+            reader = command.ExecuteReader();
+            ArrayList arrayList = new ArrayList();
+            while (reader.Read())
+            {
+                Console.WriteLine("dupa");
+                Int64 id = (Int64)reader["id_quiz"];
+                string nazwaQuiz = (string)reader["nazwa_quizu"];
+                quiz JedenQuiz = new quiz(id,nazwaQuiz);
+                arrayList.Add(JedenQuiz);
+            }
+            return arrayList;
+
+        }
+
 
     }
 }
--- quizMVVM/View-Model/SolveQuizViewModel.cs
+++ quizMVVM/View-Model/SolveQuizViewModel.cs
@@ -1,14 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using quizMVVM.Model;
+using System.IO;
+using System.Collections;
 
 namespace quizMVVM.View_Model
 {
     internal class SolveQuizViewModel : BaseViewModel
     {
         private string _id;
+        private Model.MainModel model = new Model.MainModel();
+       static string databaseFilename = "quiz.db";
+        static private string databasePath = Path.Combine("C:\\Users\\Wojtek\\Documents\\GitHub\\qiuz-mvvm-wpf\\quizMVVM", databaseFilename);
+        static SQLiteConnection conn = new SQLiteConnection($"Data Source={databasePath};Version=3");
+
+        public SolveQuizViewModel()
+        {
+            Console.WriteLine(conn);
+            ArrayList array = new ArrayList();
+          array=  MainModel.ShowAllQuizes(conn);
+            conn.Close();
+        }
+
         public string Id 
         {
             get { return this._id; }
@@ -19,6 +38,9 @@ namespace quizMVVM.View_Model
                     this._id = value;
                     this.OnPropertyChanged(nameof(Id));
                     Console.WriteLine(_id);
+
+               
+
                 }
             } 
         }
